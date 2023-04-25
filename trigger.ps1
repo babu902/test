33628a4bb6612a4765350ca5711ce343699e53aa
babu902 <127922325+babu902@users.noreply.github.com>
2023-04-25 09:03:12 +0530

    Update trigger.ps1

--- trigger.ps1
+++ trigger.ps1
@@ -1,23 +1,23 @@
-jobs:
-- job: my_job
-  steps:
-  - script: echo "This job runs before MyOtherPipeline."
-  - task: PowerShell@2
-    inputs:
-      targetType: 'inline'
-      script: |
-        $token = "$(System.AccessToken)"
-        $url = "$(System.TeamFoundationCollectionUri)/$(System.TeamProject)/_apis/pipelines/$(my_other_pipeline)/runs?api-version=6.0-preview.1"
+$orgName = "your-organization-name"
+$projectName = "your-project-name"
+$personalAccessToken = "your-personal-access-token"
 
-        do {
-          Start-Sleep -s 10
-          $response = Invoke-RestMethod -Uri $url -Method Get -Headers @{Authorization = "Bearer $token"}
-          $status = $response.value[0].status
-        } while ($status -in @('inProgress', 'notStarted'))
+$uri = "https://dev.azure.com/$orgName/$projectName/_apis/build/builds?api-version=7.0"
 
-        if ($status -eq 'succeeded') {
-          echo "MyOtherPipeline succeeded."
-        } else {
-          echo "MyOtherPipeline failed."
-        }
-  - script: echo "This job runs after MyOtherPipeline completes."
+$body = @{
+    definition = @{
+        id = "pipeline-definition-id"
+    }
+    parameters = '{
+        "parameter-name": "parameter-value"
+    }'
+}
+
+$headers = @{
+    Authorization = "Basic $([System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($personalAccessToken)")))
+    'Content-Type' = 'application/json'
+}
+
+$response = Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -Body ($body | ConvertTo-Json -Depth 10)
+
+$response
