c69f83be5943aa6c9f79fdd6e4942ea0843f32dd
babu902 <127922325+babu902@users.noreply.github.com>
2023-04-26 10:15:24 +0530

    Update pat.ps1

--- pat.ps1
+++ pat.ps1
@@ -3,35 +3,39 @@ $organizationName = "your-org-name"
 $projectName = "your-project-name"
 $repoName = "your-repo-name"
 
-# Set the personal access token (PAT) for your Azure DevOps account
+# Set the maximum number of commits to retrieve
+$top = 1
+
+# Set the REST API endpoint to get the latest commit
+$uri = "https://dev.azure.com/$organizationName/$projectName/_apis/git/repositories/$repoName/commits?searchCriteria.$top=$top&api-version=7.0"
+
+# Create an Azure DevOps REST API authentication header using a Personal Access Token (PAT)
 $pat = "your-pat"
-
-# Set the branch to search for changes
-$branch = "main"
-
-# Set the REST API endpoint to get the latest commit for the specified branch
-$uri = "https://dev.azure.com/$organizationName/$projectName/_apis/git/repositories/$repoName/commits?branch=$branch&$top=1&api-version=7.0"
-
-# Create an Azure DevOps REST API authentication header using your PAT
-$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($pat)"))
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$pat"))
 $headers = @{
     Authorization = "Basic $base64AuthInfo"
 }
 
-# Invoke the Azure DevOps REST API to get the latest commit for the specified branch
+# Invoke the Azure DevOps REST API to get the latest commit
 $response = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
 
-# Get the latest commit ID from the response
-$latestCommitId = $response.value.commitId
+# Get the commit ID for the latest commit
+$commitId = $response.value.commitId
+
+# Set the maximum number of changes to retrieve
+$top = 1000
+
+# Set the number of changes to skip
+$skip = 0
 
 # Set the REST API endpoint to get the list of changes for the latest commit
-$uri = "https://dev.azure.com/$organizationName/$projectName/_apis/git/repositories/$repoName/commits/$latestCommitId/changes?api-version=7.0"
+$uri = "https://dev.azure.com/$organizationName/$projectName/_apis/git/repositories/$repoName/commits/$commitId/changes?top=$top&skip=$skip&api-version=7.0"
 
 # Invoke the Azure DevOps REST API to get the list of changes for the latest commit
 $response = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
 
 # Print the list of changed files
-Write-Host "The following files were changed in the latest commit:"
+Write-Host "The following files were changed in the latest commit ($commitId):"
 foreach ($change in $response.value) {
     Write-Host $change.item.path
 }
